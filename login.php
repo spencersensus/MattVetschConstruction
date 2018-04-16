ccb07f04fd53f21afc0b429636fd5ba318afc46e
spencersensus <spencerwoods@Spencers-MacBook-Pro.local>
2018-04-16 17:17:22 -0600

    new

--- login.php
+++ login.php
@@ -2,9 +2,8 @@
     <html>
 <head>
     <link href="login.css" rel="stylesheet" type="text/css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script> 
         $(function(){
         $(document).tooltip();
